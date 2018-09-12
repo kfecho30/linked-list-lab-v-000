62a4b9a7a6160f1f3fdcf7994fd9bfa9b22aec8a
Kelsey Fecho <kfecho30@gmail.com>
2018-09-12 17:05:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function next(head, coll){
   let nextNode = head['next']
   return coll[nextNode]
 }
+
+function nodeAt(index, list, coll) {
+  let head = coll[list]
+  for (let i = 0; i < index; i++) {
+    next(head, coll)
+  }
+}
